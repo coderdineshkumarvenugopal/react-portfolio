524d75b75b7ed5db59a93d6d7e094b4361567b0c
DINESH KUMAR VENUGOPAL <108514601+coderdineshkumarvenugopal@users.noreply.github.com>
2023-04-20 18:19:29 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,139 @@
+const logotext = "DINESH";
+const meta = {
+    title: "DINESH KUMAR",
+    description: "I’m Dinesh Kumar Web Developer _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Dinesh Kumar",
+    animated: {
+        first: "I am an enthusiastic person",
+        second: "I code cool websites",
+        third: "I develope mobile apps and websites",
+    },
+    description: "Founder of DK TECHNOLOGIES,Professional app and web developer with 3+years of experience in technological fields",
+    your_img_url: "https://ibb.co/gWFcY4b",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: "I am Dinesh.I create websites,Mobile applications and may more.i have strong creative and analytical skills!!!",
+};
+const worktimeline = [{
+        jobtitle: "HANDIFIT",
+        where: "SKCET",
+        date: "2022",
+    },
+    {
+        jobtitle: "SLUM DEVELOPMENT",
+        where: "GAZIABHAD",
+        date: "2022",
+    },
+    {
+        jobtitle: "PREGMED-PREGNANCY RELATED",
+        where: "SKCET",
+        date: "2023",
+    },
+    {
+        jobtitle: "WOMEN SAFETY PROJECT",
+        where: "SKCET",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "HTML & CSS",
+        value: 90,
+    },
+    {
+        name: "REACT NATIVE",
+        value: 85,
+    },
+    {
+        name: "JAVASCRIPT",
+        value: 80,
+    },
+    {
+        name: "REACT",
+        value: 90,
+    },
+    {
+        name: "C & C++",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I have done a impressive UI/UX Design conducted by IIT MADRAS",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I have created a Mobile Application conducted by KIET GAZIABHAD",
+    },
+    {
+        title: "Blogging",
+        description: "I used to write my own Thoughts as pen and paper which is Quite Impressive",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "HANDIFIT",
+        link: "https://github.com/",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "WOMEN SAFETY",
+        link: "https://github.com/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "PREGMED",
+        link: "https://github.com/",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "FIRE ALARM",
+        link: "https://github.com/",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: " I SECURE",
+        link: "https://github.com/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "SAFETY FY",
+        link: "https://github.com/",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "dineshkumarvenugopal3@gmail.com",
+    YOUR_FONE: "+91 90430 19630",
+    description: "Feel free to contact me!!! ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata, 
+    contactConfig,
+    socialprofils,
+    logotext,
+};
